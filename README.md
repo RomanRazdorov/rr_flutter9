ee69a4505f591a8f597abbdbb18445e6ba3e0e9f
RomanRazdorov <danka.kuzka@mail.ru>
2024-09-18 13:40:33 +0300

    Project initializing

---
+++ README.md
@@ -0,0 +1,3 @@
+# rr_flutter9
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/rr_flutter9/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rr_flutter9
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
